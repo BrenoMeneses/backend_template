81f54f016cdecea3df930c36f7a014426df351c3
breno meneses <brenomenesesdiaz@gmail.com>
2025-08-21 00:01:59 -0300

    organização de arquivos

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
-import express from 'express';
+import app from "./app.js";
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
